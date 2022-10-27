f6b420d1c79e3ce03fefb1e5717449355da2cb87
Priyanshu Singh <36508048+priyanshusingh92@users.noreply.github.com>
2022-10-27 22:17:23 +0530

    update

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,13 +1,9 @@
-import "./Footer.scss";
-import {Fade} from "react-reveal";
-
 export default function Footer() {
 
   return (
-    <Fade bottom duration={1000} distance="5px">
-      <div className="footer-div">
+      <div>
     
       </div>
-    </Fade>
+ 
   );
 }
