03202aa9a5288ca8d06bacfe77b829e372a6a33b
priyanshusingh92 <singhpriyanshu772@gmail.com>
2022-10-28 11:02:43 +0530

    profile

--- src/containers/profile/Profile.js
+++ src/containers/profile/Profile.js
@@ -1,6 +1,5 @@
 import React, {useState, useEffect, lazy, Suspense} from "react";
 import {openSource} from "../../portfolio";
-import Contact from "../contact/Contact";
 import Loading from "../loading/Loading";
 
 const renderLoader = () => <Loading />;
