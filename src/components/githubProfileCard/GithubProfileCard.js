b12d80e865bb53f21f8bb8938488ea975d8d9549
priyanshusingh92 <singhpriyanshu772@gmail.com>
2022-10-28 10:11:46 +0530

    build fix

--- src/components/githubProfileCard/GithubProfileCard.js
+++ src/components/githubProfileCard/GithubProfileCard.js
@@ -50,7 +50,7 @@ export default function GithubProfileCard({prof}) {
           </div>
           <div className="image-content-profile">
             <img
-              src={require("./assets/images/photo.jpg")}
+              src="./photo.jpg"
               alt={prof.name}
               className="profile-image"
             />
